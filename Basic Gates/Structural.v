fa52682b780b1ee6a9f3456de1cc144b462d322c
Satindrakumar <146068026+Satindrakumar@users.noreply.github.com>
2025-05-27 15:36:21 +0530

    Update Structural.v

--- Basic Gates/Structural.v
+++ Basic Gates/Structural.v
@@ -1,10 +1,10 @@
 module data_basicgates(output out_and,out_or,out_nand,out_nor,out_not,out_xor,out_xnor,
                        input a,b);
-and  g1(out_and, a, b);
-or   g2(out_or, a, b);
+and g1(out_and, a, b);
+or g2(out_or, a, b);
 nand g3(out_nand, a, b);
-nor  g4(out_nor, a, b);
-not  g5(out_not, a);       
-xor  g6(out_xor, a, b);
+nor g4(out_nor, a, b);
+not g5(out_not, a);       
+xor g6(out_xor, a, b);
 xnor g7(out_xnor, a, b);
 endmodule
